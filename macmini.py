1caeeed39e5b763cd2e6818c1f19bc5464f7f883
Kevin <kevin@hafur.com>
2021-04-29 22:38:11 +0200

    mac mini version fixed

--- macmini.py
+++ macmini.py
@@ -6,7 +6,6 @@ import shutil
 from datetime import date, timedelta
 from exif import Image
 
-from wand.image import Image
 
 def main():
 
@@ -20,13 +19,14 @@ def main():
     yes_day = yesterday.strftime('%d')
 
     # Start exporting photos from this date onwards
-    yes_str = '20201101'
+    yes_str = '20210426'
 
     photosdb = osxphotos.PhotosDB()
     photos = photosdb.photos(from_date=datetime.datetime.strptime(yes_str, '%Y%m%d'))
 
-
+   
     for p in photos:
+        print(p.original_filename, 'shared: ', p.shared)
         if not p.shared or p.shared == None:
         # The folder structure - right now it is root / year / month
             # if hasattr(p.exif_info, 'camera_model'):
@@ -36,9 +36,10 @@ def main():
             fold_struc_mov = 'export/%Y/%m/movies/'
             fold_struc_no_model='export/%Y/%m/none/'
 
-            prep_dir = 'export/tmp'
+            prep_dir = 'export/tmp/'
 
             os.makedirs(p.date.strftime(fold_struc_mov), exist_ok=True)
+            os.makedirs(p.date.strftime(fold_struc_no_model), exist_ok=True)
             mov_dir = p.date.strftime(fold_struc_mov)
             os.makedirs(p.date.strftime(prep_dir), exist_ok=True)
             prep_dir_str = p.date.strftime(prep_dir)
@@ -50,38 +51,30 @@ def main():
                 p.export(mov_dir, p.date.strftime('%Y%m%d') + '_' + p.original_filename)
             else:
                 if p.hasadjustments:
-                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename, edited=True)
+                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename, edited=True, use_photos_export=True)
                 else:
-                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename)
+                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename, use_photos_export=True)
+            
+            print('check: ', new_name)
+            if new_name.endswith('.HEIC'):
+                os.system('heic2jpg -s')
+                print('converted HEIC!')
+            elif new_name.lower().endswith('.png'):
+                shutil.move(prep_dir + '/' + new_name, p.date.strftime(fold_struc_no_model))
 
             if not p.ismovie:
                 basepath = prep_dir
-                with open(basepath + '/' + new_name, 'rb') as image_file:
-                    my_image = Image(image_file)
-                    # print(my_image.model)
-                    try:
-                        fold_struc = fold_struc + my_image.model
-                        os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
-                        tar_dir = p.date.strftime(fold_struc) + '/'
-                        shutil.move(prep_dir_str + '/' + new_name, tar_dir)
-                    except AttributeError:
-                        os.makedirs(p.date.strftime(fold_struc_no_model), exist_ok=True)
-                        tar_dir = p.date.strftime(fold_struc) + 'none/'
-                        shutil.move(prep_dir_str + '/' + new_name, tar_dir)
-
-            # if not p.ismovie:
-            #     basepath = prep_dir
-            #     with os.scandir(basepath) as entries:
-            #         for entry in entries:
-            #             if entry.is_file():
-            #                 # print(entry.name)
-            #                 with open(entry, 'rb') as image_file:
-            #                     my_image = Image(image_file)
-            #                     # print(my_image.model)
-            #                     fold_struc = fold_struc + my_image.model
-            #                     os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
-            #                     tar_dir = p.date.strftime(fold_struc) + '/'
-            #                     shutil.move(prep_dir_str + '/' + new_name, tar_dir)
+                with os.scandir(basepath) as entries:
+                    for entry in entries:
+                        if entry.is_file():
+                            print(entry.name)
+                            with open(entry, 'rb') as image_file:
+                                my_image = Image(image_file)
+                                # print(my_image.model)
+                                fold_struc = fold_struc + my_image.model
+                                os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
+                                tar_dir = p.date.strftime(fold_struc) + '/'
+                                shutil.move(prep_dir_str + '/' + entry.name, tar_dir)
 
             # print(
             #       p.original_filename)
