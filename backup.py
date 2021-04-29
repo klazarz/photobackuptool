c46c00f6286ccd6fcd659f6b96ef9ee450d1cd14
Kevin <kevin@hafur.com>
2021-04-29 09:24:49 +0200

    added fix for photos 3

---
+++ backup.py
@@ -0,0 +1,57 @@
+import osxphotos
+import datetime
+import os
+
+from datetime import date, timedelta
+
+def main():
+
+
+    # This is yesterday's date - relevant for daily exports
+    today = date.today()
+    yesterday = today - timedelta(days = 1)
+    yes_str = yesterday.strftime('%Y%m%d')
+
+    # Start exporting photos from this date onwards
+    yes_str = '20200101'
+
+    photosdb = osxphotos.PhotosDB()
+    photos = photosdb.photos(from_date=datetime.datetime.strptime(yes_str, '%Y%m%d'))
+
+
+    for p in photos:
+        if not p.shared:
+        # The folder structure - right now it is root / year / month
+            if hasattr(p.exif_info, 'camera_model'):
+                fold_struc = "/Volumes/YOGI/photos/%Y/%m/" + str(p.exif_info.camera_model)
+            else:
+                fold_struc = "/Volumes/YOGI/photos/%Y/%m/none"
+
+            os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
+
+           
+            if hasattr(p.exif_info, 'camera_model'):
+                if p.hasadjustments:
+                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_' +str(p.exif_info.camera_model) + '_' + p.original_filename, edited=True)
+                else:
+                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_' + str(p.exif_info.camera_model) + '_' + p.original_filename)
+            else:
+                if p.hasadjustments:
+                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_none_' + p.original_filename, edited=True)
+                else:
+                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_none_' + p.original_filename)
+
+            print(
+                  p.original_filename
+            #     p.shared,
+            #     p.exif_info.camera_model,
+            #     # p.path_edited,
+            #     # p.path,
+            #     p.hasadjustments
+            #     # p.date.strftime('%Y%m%d'),
+            #     # file_extension,
+            #     # p.date.strftime('%Y%m%d') + str(file_extension)
+            )
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import osxphotos
 import datetime
 import os
+import shutil
 
 from datetime import date, timedelta
+from exif import Image
 
 def main():
 
@@ -11,38 +13,60 @@ def main():
     today = date.today()
     yesterday = today - timedelta(days = 1)
     yes_str = yesterday.strftime('%Y%m%d')
+    yes_year = yesterday.strftime('%Y')
+    yes_month = yesterday.strftime('%m')
+    yes_day = yesterday.strftime('%d')
 
     # Start exporting photos from this date onwards
-    yes_str = '20200101'
+    yes_str = '20210427'
 
     photosdb = osxphotos.PhotosDB()
     photos = photosdb.photos(from_date=datetime.datetime.strptime(yes_str, '%Y%m%d'))
 
 
     for p in photos:
-        if not p.shared:
+        if not p.shared or p.shared == None:
         # The folder structure - right now it is root / year / month
-            if hasattr(p.exif_info, 'camera_model'):
-                fold_struc = "/Volumes/YOGI/photos/%Y/%m/" + str(p.exif_info.camera_model)
-            else:
-                fold_struc = "/Volumes/YOGI/photos/%Y/%m/none"
+            # if hasattr(p.exif_info, 'camera_model'):
+            #     fold_struc = "export/%Y/%m/" + str(p.exif_info.camera_model)
+            # else:
+            fold_struc = 'export/%Y/%m/'
+            fold_struc_mov = 'export/%Y/%m/movies'
 
-            os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
+            prep_dir = 'export/tmp'
 
+            os.makedirs(p.date.strftime(fold_struc_mov), exist_ok=True)
+            mov_dir = p.date.strftime(fold_struc_mov)
+            os.makedirs(p.date.strftime(prep_dir), exist_ok=True)
+            prep_dir_str = p.date.strftime(prep_dir)
+            
            
-            if hasattr(p.exif_info, 'camera_model'):
-                if p.hasadjustments:
-                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_' +str(p.exif_info.camera_model) + '_' + p.original_filename, edited=True)
-                else:
-                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_' + str(p.exif_info.camera_model) + '_' + p.original_filename)
+            new_name = p.date.strftime('%Y%m%d') + '_' + p.original_filename
+
+            if p.ismovie:
+                p.export(mov_dir, p.date.strftime('%Y%m%d') + '_' + p.original_filename)
             else:
                 if p.hasadjustments:
-                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_none_' + p.original_filename, edited=True)
+                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename, edited=True)
                 else:
-                    p.export(p.date.strftime(fold_struc), p.date.strftime('%Y%m%d') + '_none_' + p.original_filename)
+                    p.export(p.date.strftime(prep_dir), p.date.strftime('%Y%m%d') + '_' + p.original_filename)
 
-            print(
-                  p.original_filename
+
+            basepath = prep_dir
+            with os.scandir(basepath) as entries:
+                for entry in entries:
+                    if entry.is_file():
+                        # print(entry.name)
+                        with open(entry, 'rb') as image_file:
+                            my_image = Image(image_file)
+                            # print(my_image.model)
+                            fold_struc = fold_struc + my_image.model
+                            os.makedirs(p.date.strftime(fold_struc), exist_ok=True)
+                            tar_dir = p.date.strftime(fold_struc) + '/'
+                            shutil.move(prep_dir_str + '/' + new_name, tar_dir)
+
+            # print(
+            #       p.original_filename)
             #     p.shared,
             #     p.exif_info.camera_model,
             #     # p.path_edited,
@@ -51,7 +75,8 @@ def main():
             #     # p.date.strftime('%Y%m%d'),
             #     # file_extension,
             #     # p.date.strftime('%Y%m%d') + str(file_extension)
-            )
+            # )
+
 
 if __name__ == "__main__":
     main()
