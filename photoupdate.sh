f15d2bf97eab5233d9b63c6064765900b1dd52e6
Kevin <kevin@hafur.com>
2021-04-30 22:21:55 +0200

    final touches

--- photoupdate.sh
+++ photoupdate.sh
@@ -1,2 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
+PATH=/Users/kevin/anaconda/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/Server.app/Contents/ServerRoot/usr/bin:/Applications/Server.app/Contents/ServerRoot/usr/sbin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Users/kevin/Documents/python_projects/photobackuptool
+
 python3.9 /Users/kevin/Documents/python_projects/photobackuptool/macmini.py
